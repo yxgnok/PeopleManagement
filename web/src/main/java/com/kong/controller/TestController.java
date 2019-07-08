e2cda2508dc4fb1a42bd09e442b1f029ebe4e263
XiangyanKong <kongxy1101@163.com>
2019-07-09 00:04:26 +0800

    用testController和show.jsp测试前面的配置是否正确

---
+++ web/src/main/java/com/kong/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.kong.controller;
+
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller("testController")
+public class TestController {
+    // /test/show.do  /show.jsp
+    public void show(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("NAME","张三");
+        request.getRequestDispatcher("../show.jsp").forward(request, response);
+
+    }
+}
--- web/src/main/webapp/show.jsp
+++ web/src/main/webapp/show.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-Hello.World!
+Hello.${NAME}!
 </body>
 </html>
